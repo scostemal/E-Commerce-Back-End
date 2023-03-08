47f37fa81b1ae907c6aca2c4f5526c4501dc1a5b
[Scott Malone] <[scott.malone62@gmail.com]>
2023-03-07 23:11:54 -0600

    created the relationships of the models

--- models/index.js
+++ models/index.js
@@ -5,12 +5,32 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'SET NULL'
+});
 
 // Categories have many Products
 
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
+
 // Products belongToMany Tags (through ProductTag)
 
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id',
+  unique: false,
+});
+
 // Tags belongToMany Products (through ProductTag)
+
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id',
+  unique: false,
+});
 
 module.exports = {
   Product,
